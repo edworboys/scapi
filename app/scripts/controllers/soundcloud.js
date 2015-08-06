1c422240e8b4e4814f06deb884eb04aef49d5899
Ed Worboys <edworboys@gmail.com>
2015-08-06 16:47:24 +0100

    Added 'get this group button'

--- app/scripts/controllers/soundcloud.js
+++ app/scripts/controllers/soundcloud.js
@@ -8,8 +8,21 @@
  * Controller of the scapiApp
  */
 angular.module('scapiApp')
-  .controller('SoundcloudCtrl', ['$scope', 'sc', function ($scope, sc) {
-    sc.success(function(data) {
-      $scope.groupInfo = data;
-    });
+  .controller('SoundcloudCtrl', ['$scope', 'sc', function ($scope, sc, groupTracks) {
+
+    $scope.groupId = 49;
+
+    $scope.getGroup = function() {
+      sc.group($scope.groupId).success(function (data) {
+        $scope.groupInfo = data;
+      });
+
+      sc.tracks($scope.groupId).success(function (data) {
+        $scope.tracks = data;
+
+      });
+
+    }
+
+
   }]);
--- app/scripts/services/sc.js
+++ app/scripts/services/sc.js
@@ -8,13 +8,20 @@
  * Service in the scapiApp.
  */
 angular.module('scapiApp')
-  .service('sc', ['$http', function ($http) {
-    return $http.get('http://api.soundcloud.com/groups/49?client_id=ed3fb73ea5aafe15e1d9e013e93f7d05')
-      .success(function(data) {
-        return data;
-      })
-      .error(function(err) {
-        return err;
-      });
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  }]);
+  .factory('sc', ['$http', function ($http) {
+    var clientIdParam = '?client_id=ed3fb73ea5aafe15e1d9e013e93f7d05';
+
+    return {
+      group: function (groupId) {
+        return $http.get('http://api.soundcloud.com/groups/' + groupId + clientIdParam);
+      },
+      tracks: function(groupId) {
+        return $http.get('http://api.soundcloud.com/groups/' + groupId + "/tracks" + clientIdParam + "&limit=50" );
+      },
+      user: function (userId) {
+        return $http.get('http://api.soundcloud.com/users/' + userId + clientIdParam);
+      }
+
+    };
+  }])
+
