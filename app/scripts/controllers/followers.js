b9b73a818469673882cd83352692eca7d01fd80f
Ed Worboys <ed@audiu.net>
2015-08-10 19:26:46 +0100

    merged two input boxes into one

---
+++ app/scripts/controllers/followers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scapiApp.controller:FollowersCtrl
+ * @description
+ * # FollowersCtrl
+ * Controller of the scapiApp
+ */
+angular.module('scapiApp')
+  .controller('FollowersCtrl', function ($scope) {
+
+  });
